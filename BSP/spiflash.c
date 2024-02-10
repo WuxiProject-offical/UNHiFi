4bd6b664b2fd4e37b50276e7f1d5f173804963ee
風は吹いてるか? <50514886+rinscr3003@users.noreply.github.com>
2024-02-10 13:34:59 +0800

    add spiflash read

--- BSP/spiflash.c
+++ BSP/spiflash.c
@@ -9,3 +9,35 @@ uint8_t spiflash_Init(void)
 {
     return 0;
 }
+
+void spiflash_Read(uint32_t addr, uint8_t *buf, uint32_t len)
+{
+    uint8_t readSeq[4] = {0x03, (addr >> 16) & 0x000000ff, (addr >> 8) & 0x000000ff, (addr >> 0) & 0x000000ff};
+    SPIFLASH_CS_EN();
+    for (uint8_t i = 0; i < 4; i++)
+    {
+        while (RESET == spi_i2s_flag_get(SPI2, SPI_FLAG_TBE))
+        {
+            __NOP();
+        }
+        spi_i2s_data_transmit(SPI2, readSeq[i]);
+    }
+    for (uint32_t i = 0; i < len; i++)
+    {
+        while (RESET == spi_i2s_flag_get(SPI2, SPI_FLAG_TBE))
+        {
+            __NOP();
+        }
+        spi_i2s_data_transmit(SPI2, 0xff);
+        while (RESET == spi_i2s_flag_get(SPI2, SPI_FLAG_RBNE))
+        {
+            __NOP();
+        }
+        *(buf + i) = (uint8_t)spi_i2s_data_receive(SPI2);
+    }
+    while (SET == spi_i2s_flag_get(SPI2, SPI_FLAG_TRANS))
+    {
+        __NOP();
+    }
+    SPIFLASH_CS_DIS();
+}
--- BSP/spiflash.h
+++ BSP/spiflash.h
@@ -4,6 +4,6 @@
 #include "gd32f30x.h"
 
 uint8_t spiflash_Init(void);
+void spiflash_Read(uint32_t addr,uint8_t *buf,uint32_t len);
 
-
-#endif /* AT24CXX_H */
+#endif

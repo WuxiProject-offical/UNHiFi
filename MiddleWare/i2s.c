fc723dfe9ff5455e36010bcf8918e9ba2722e716
風は吹いてるか? <50514886+rinscr3003@users.noreply.github.com>
2024-02-13 13:20:18 +0800

    i2s trim

--- MiddleWare/i2s.c
+++ MiddleWare/i2s.c
@@ -16,11 +16,22 @@ void i2s_config(void)
     gpio_init(GPIOB, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_15);
     gpio_init(GPIOC, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_6);
 
+    // SPI/I2S config
     spi_i2s_deinit(SPI1);
-
     i2s_init(SPI1, I2S_MODE_MASTERTX, I2S_STD_PHILLIPS, I2S_CKPL_HIGH);
     i2s_psc_config(SPI1, I2S_AUDIOSAMPLE_48K, I2S_FRAMEFORMAT_DT16B_CH16B, I2S_MCKOUT_ENABLE);
     i2s_enable(SPI1);
+}
+
+void i2s_SetFs(uint32_t fsopt)
+{
+    SPI_I2SPSC(SPI1) = (uint32_t)(fsopt | I2S_MCKOUT_ENABLE);
+}
+
+void i2s_SetFrameFormat(uint32_t i2s_frameformat)
+{
+    SPI_I2SCTL(SPI1) &= (uint32_t)(~(SPI_I2SCTL_DTLEN | SPI_I2SCTL_CHLEN));
+    SPI_I2SCTL(SPI1) |= (uint32_t)i2s_frameformat;
 }
 
 void i2s_DMASetSrc(void *srcaddr, uint32_t len)
@@ -32,7 +43,6 @@ void i2s_DMASetSrc(void *srcaddr, uint32_t len)
 void i2s_DMAConfig32(uint32_t *srcbuf, uint32_t len)
 {
     dma_parameter_struct dma_init_struct;
-
     /* SPI1 transmit dma config: DMA0,DMA_CH4  */
     dma_deinit(DMA0, DMA_CH4);
     dma_init_struct.periph_addr = (uint32_t)&SPI_DATA(SPI1);
@@ -53,7 +63,6 @@ void i2s_DMAConfig32(uint32_t *srcbuf, uint32_t len)
 void i2s_DMAConfig16(uint16_t *srcbuf, uint32_t len)
 {
     dma_parameter_struct dma_init_struct;
-
     /* SPI1 transmit dma config: DMA0,DMA_CH4  */
     dma_deinit(DMA0, DMA_CH4);
     dma_init_struct.periph_addr = (uint32_t)&SPI_DATA(SPI1);
@@ -73,12 +82,12 @@ void i2s_DMAConfig16(uint16_t *srcbuf, uint32_t len)
 
 void i2s_DMAStart()
 {
-    dma_channel_enable(DMA0, DMA_CH4);
-    spi_dma_enable(SPI1, SPI_DMA_TRANSMIT);
+    DMA_CHCTL(DMA0, DMA_CH4) |= DMA_CHXCTL_CHEN;
+    SPI_CTL1(SPI1) |= (uint32_t)SPI_CTL1_DMATEN;
 }
 
 void i2s_DMAStop()
 {
-    dma_channel_disable(DMA0, DMA_CH4);
-    spi_dma_disable(SPI1, SPI_DMA_TRANSMIT);
+    DMA_CHCTL(DMA0, DMA_CH4) &= ~DMA_CHXCTL_CHEN;
+    SPI_CTL1(SPI1) &= (uint32_t)(~SPI_CTL1_DMATEN);
 }
--- MiddleWare/i2s.h
+++ MiddleWare/i2s.h
@@ -3,7 +3,14 @@
 
 #include "gd32f30x.h"
 
+#define I2S_FSOPT_44K1 ((uint32_t)(6 + 0x100))
+#define I2S_FSOPT_48k ((uint32_t)(6))
+#define I2S_FSOPT_96k ((uint32_t)(3))
+#define I2S_FSOPT_192k ((uint32_t)(1 + 0x100))
+
 void i2s_config(void);
+void i2s_SetFs(uint32_t fsopt);
+void i2s_SetFrameFormat(uint32_t i2s_frameformat);
 void i2s_DMASetSrc(void *srcaddr, uint32_t len);
 void i2s_DMAConfig32(uint32_t *srcbuf, uint32_t len);
 void i2s_DMAConfig16(uint16_t *srcbuf, uint32_t len);

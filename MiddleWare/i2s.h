b4ffa5e0e976e6b1f967724bdadd6b2e4dc6d1c9
風は吹いてるか? <50514886+rinscr3003@users.noreply.github.com>
2024-02-13 13:26:37 +0800

    Update i2s.h

--- MiddleWare/i2s.h
+++ MiddleWare/i2s.h
@@ -4,9 +4,9 @@
 #include "gd32f30x.h"
 
 #define I2S_FSOPT_44K1 ((uint32_t)(6 + 0x100))
-#define I2S_FSOPT_48k ((uint32_t)(6))
-#define I2S_FSOPT_96k ((uint32_t)(3))
-#define I2S_FSOPT_192k ((uint32_t)(1 + 0x100))
+#define I2S_FSOPT_48K ((uint32_t)(6))
+#define I2S_FSOPT_96K ((uint32_t)(3))
+#define I2S_FSOPT_192K ((uint32_t)(1 + 0x100))
 
 void i2s_config(void);
 void i2s_SetFs(uint32_t fsopt);
